308b5a10470345c02af1eb0b44c987d13c5271e2
Taian <taian.f.feitosa@gmail.com>
2026-07-24 19:01:37 +0100

    fix numlock (#18)
    # Description
    
    <!-- What does this PR do? -->
    
    ## Type of change
    
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Refactor
    - [ ] Config change
    - [ ] Other
    
    ## Checklist
    
    - [ ] Tested locally (`nixup`)
    - [ ] No secrets or keys committed
    - [ ] Pre-commit hooks pass

--- home.nix
+++ home.nix
@@ -43,6 +43,14 @@
   };
 
   xdg.configFile = {
+    "cosmic/com.system76.CosmicComp/v1/keyboard_config" = {
+      force = true;
+      text = ''
+        (
+            numlock_state: BootOn,
+        )
+      '';
+    };
     "zed/settings.json".source = ./config/zed/settings.json;
     "topgrade.toml".source = ./config/topgrade.toml;
     "opencode/opencode.json".source = ./config/opencode/opencode.json;
