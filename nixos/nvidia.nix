a6ecb64e58e90c412b96a223c9b985ac2f362ba2
Taian Fonseca Feitosa <taian.f.feitosa@gmail.com>
2026-07-24 09:47:42 +0100

    Update nvidia.nix

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -2,7 +2,10 @@
 { config, ... }:
 
 {
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [
+    "modesetting"
+    "nvidia"
+  ];
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
@@ -12,5 +15,18 @@
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
   };
+  services.switcherooControl.enable = true;
 }
